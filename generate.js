99c0b1b04d3bd91ee973844b1037df290890f347
AbdulazizYas <abdulazizyass@gmail.com>
2023-07-17 10:31:34 +0300

    edit after postman collections

--- generate.js
+++ generate.js
@@ -1,44 +1,146 @@
 const { faker } = require("@faker-js/faker");
 
-function generate() {
-  const studentsNum = 34;
-  const coursesNum = 18;
-
-  const students = [];
-
-  for (let i = 0; i < studentsNum; i++) {
-    students.push({
-      id: i + 1,
-      name: faker.person.fullName(),
-      email: faker.internet.exampleEmail(),
-      joinedDate: faker.date.recent({ days: 14 }),
-      progress: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
-      materialsWatched: faker.number.int({ min: 0, max: 45 }),
-      courseId: faker.number.int({ min: 1, max: coursesNum }),
-    });
-  }
-
-  const courses = [];
-
-  for (let i = 0; i < coursesNum; i++) {
-    courses.push({
-      id: i + 1,
-      name: faker.commerce.productName(),
-      price: faker.commerce.price({
-        min: 50,
-        max: 450,
-        dec: 0,
-        symbol: "SAR",
-      }),
-      status: faker.helpers.arrayElement(["published", "draft", "unlisted"]),
-    });
-  }
-  const data = {
-    courses,
-    students,
+function generateTaxonomies(count) {
+  const taxos = {
+    data: [],
   };
 
-  return data;
+  for (let i = 0; i < count; i++) {
+    taxos.data.push({
+      id: i + 1,
+      name: faker.commerce.department(),
+      icon: null,
+      description: null,
+      type: faker.number.int({ min: 1, max: 4 }),
+    });
+  }
+
+  return taxos;
+}
+
+function generateCourses(count, cats) {
+  const courses = {
+    data: [],
+  };
+  const levels = cats.data.filter((cat) => cat.type === 3);
+
+  for (let i = 0; i < count; i++) {
+    courses.data.push({
+      id: i + 1,
+      title: faker.commerce.productName(),
+      url: faker.internet.url(),
+      checkout_url: faker.internet.url(),
+      thumbnail: faker.internet.url(),
+      summary: faker.lorem.sentence(7),
+      description: faker.lorem.sentence(10),
+      requirements: faker.lorem.sentences(5).split(" "),
+      meta_keywords: faker.lorem.words(5).split(" "),
+      meta_description: faker.lorem.paragraph(2),
+      eligible_for_certificate: false,
+      duration: "3583.00",
+      price: faker.commerce.price({ min: 50, max: 500 }),
+      sales_price: faker.commerce.price({ min: 50, max: 500 }),
+      avg_rating: 5,
+      contents_count: 6,
+      enrollments_count: 2,
+      slug: "programming",
+      status: 1,
+      is_started: true,
+      in_stock: true,
+      publish_at: "2022-10-05T12:30:00.000000Z",
+      updated_at: "2022-11-20T14:01:52.000000Z",
+      created_at: "2022-10-05T12:05:02.000000Z",
+      instructors: [],
+      category: {
+        id: 5,
+        name: "غير مصنف",
+        icon: null,
+        description: null,
+      },
+      subcategory: faker.helpers.arrayElement(levels),
+      certification: {
+        enabled: false,
+      },
+      options: {
+        reviews_enabled: true,
+      },
+      meta: {
+        early_access: true,
+        show_enrollments_count: true,
+        show_content_instructor: true,
+        disable_comments: false,
+        close_enrollments: false,
+        resubmit_assignment: false,
+        can_retake_exam: true,
+      },
+      page: null,
+    });
+  }
+
+  return courses;
+}
+
+function generateEnrollements(count, courses) {
+  const enrolls = {
+    data: [],
+  };
+
+  for (let i = 0; i < count; i++) {
+    enrolls.data.push({
+      id: i + 1,
+      course: faker.helpers.arrayElement(courses.data),
+      member: {
+        id: faker.number.int({ min: 1, max: 14 }),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        name: faker.person.fullName(),
+        username: faker.internet.displayName(),
+        avatar: faker.internet.url(),
+        bio: null,
+        job_title: null,
+        education: null,
+        url: faker.internet.url(),
+        national_id: null,
+        country_code: "KSA",
+        currency: "SAR",
+        gender: faker.helpers.arrayElement(["male", "female"]),
+        skills: null,
+        email: faker.internet.email(),
+        phone_code: 966,
+        phone: "537812391",
+        international_phone: "966537812391",
+        dob: "2022-05-12",
+        email_verified_at: null,
+        status: 1,
+        last_seen_at: "2022-12-29T10:02:11.000000Z",
+        updated_at: "2022-11-21T09:48:35.000000Z",
+        created_at: "2022-05-10T06:14:54.000000Z",
+      },
+      percentage_completed: faker.number.int({ min: 0, max: 100 }).toString(),
+      started_at: "2022-12-20T15:32:11.000000Z",
+      completed_at: null,
+      updated_at: "2022-12-25T13:11:29.000000Z",
+      created_at: "2022-12-20T15:30:12.000000Z",
+    });
+  }
+
+  return enrolls;
+}
+
+function generate() {
+  const taxosCount = 74;
+  const coursesCount = 110;
+  const enrollsCount = 68;
+
+  const taxonomies = generateTaxonomies(taxosCount);
+  const courses = generateCourses(coursesCount, taxonomies);
+  const enrollments = generateEnrollements(enrollsCount, courses);
+
+  return {
+    taxonomies,
+    courses,
+    enrollments,
+  };
 }
 
 console.log(JSON.stringify(generate(), null, 2));
